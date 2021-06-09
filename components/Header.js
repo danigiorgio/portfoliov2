5c3dfb52d3cb8117baab5b8d3c9f1f2ec64caa25
voirs <danielagiorgioe@gmail.com>
2021-06-08 22:10:26 -0300

    add underline to active links in nav

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,10 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import ThemeSwitch from "./ThemeSwitch";
+
 export default function Header() {
+  const { pathname } = useRouter();
   return (
     <header className="py-10 px-4 sm:px-6 lg:px-0">
       <nav className="flex items-center justify-between sm:flex-row max-w-3xl mx-auto">
@@ -13,22 +16,24 @@ export default function Header() {
         <ul className="flex items-center">
           <li>
             <Link href="/">
-              <a className="dark:text-gray-200 hover:text-gray-500">Home</a>
+              <a className={`dark:text-gray-200 hover:text-gray-500 ${pathname === "/" && "border-b-2"}`}>Home</a>
             </Link>
           </li>
           <li className="ml-6">
             <Link href="/about">
-              <a className="dark:text-gray-200 hover:text-gray-500">About</a>
+              <a className={`dark:text-gray-200 hover:text-gray-500 ${pathname === "/about" && "border-b-2"}`}>About</a>
             </Link>
           </li>
           <li className="ml-6">
             <Link href="/projects">
-              <a className="dark:text-gray-200 hover:text-gray-500">Projects</a>
+              <a className={`dark:text-gray-200 hover:text-gray-500 ${pathname === "/projects" && "border-b-2"}`}>
+                Projects
+              </a>
             </Link>
           </li>
           <li className="ml-6">
             <Link href="/blog">
-              <a className="dark:text-gray-200 hover:text-gray-500">Blog</a>
+              <a className={`dark:text-gray-200 hover:text-gray-500 ${pathname === "/blog" && "border-b-2"}`}>Blog</a>
             </Link>
           </li>
           <li className="ml-6">
